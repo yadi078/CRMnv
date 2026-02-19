77dfb5e1406bc4616ec037b1e957814bb061f735
columbaSilva <2236001173@utna.edu.mx>
2026-02-18 19:08:22 -0600

    solo revision

--- app/Notifications/NewUserRegisteredNotification.php
+++ app/Notifications/NewUserRegisteredNotification.php
@@ -6,7 +6,7 @@ use App\Models\User;
 use Illuminate\Notifications\Notification;
 
 class NewUserRegisteredNotification extends Notification
-
+{
     public function __construct(
         public User $registeredUser
     ) {}
